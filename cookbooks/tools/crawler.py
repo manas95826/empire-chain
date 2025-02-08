a94747fd0e6e9ad1d7d3c162ca4ac61dccfcd1fa
Manas Chopra <74074241+manas95826@users.noreply.github.com>
2025-02-08 20:38:53 +0530

    Update crawler.py

--- cookbooks/tools/crawler.py
+++ cookbooks/tools/crawler.py
@@ -2,6 +2,7 @@
 This is a simple crawler that uses the Empire Chain library to crawl a website and save the content as markdown.
 Please run the following command to install the necessary dependencies and store keys in .env:
 !pip install empire-chain crawl4ai
+!playwright install
 """
 from empire_chain.tools.crawl4ai import Crawler
 
