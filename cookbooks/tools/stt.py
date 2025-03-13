fe45183a94ec40b04ea5eb239a7911883c4f2b2b
Manas Chopra <74074241+manas95826@users.noreply.github.com>
2025-03-14 04:07:49 +0530

    Update stt.py

--- cookbooks/tools/stt.py
+++ cookbooks/tools/stt.py
@@ -1,8 +1,4 @@
 from empire_chain.stt.stt import GroqSTT
-from empire_chain.stt.stt import HuggingFaceSTT
-from dotenv import load_dotenv
-import os
-load_dotenv()
 
 stt = GroqSTT()
 text = stt.transcribe("audio.mp3")
